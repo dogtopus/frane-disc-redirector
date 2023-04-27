2eb5a7eecb74636fa95049f242a5c9730cad1b47
dogtopus <dogtopus@users.noreply.github.com>
2023-04-27 00:09:07 -0300

    Add a workaround for ExitProcess() hanging on Wine

--- agent/index.ts
+++ agent/index.ts
@@ -7,6 +7,13 @@ const FIND_GAME_DISC_SNIPPET_SIZE = FIND_GAME_DISC_SNIPPET_END.sub(FIND_GAME_DIS
 
 const EXE_NAME = 'frane.exe';
 
+const ExitProcess_p = Module.getExportByName('KERNEL32.DLL', 'ExitProcess');
+const GetCurrentProcess_p = Module.getExportByName('KERNEL32.DLL', 'GetCurrentProcess');
+const TerminateProcess_p = Module.getExportByName('KERNEL32.DLL', 'TerminateProcess');
+
+const GetCurrentProcess = new NativeFunction(GetCurrentProcess_p, 'pointer', [], 'stdcall');
+const TerminateProcess = new NativeFunction(TerminateProcess_p, 'bool', ['pointer', 'uint'], 'stdcall');
+
 const exemodule = Process.enumerateModules()[0];
 
 let new_game_disc_prefix = null;
@@ -22,6 +29,15 @@ if (new_game_disc_prefix !== null && new_game_disc_prefix.length < GAME_DISC_PRE
         writer.flush();
     })
     GAME_DISC_PREFIX.writeAnsiString(new_game_disc_prefix);
+
+    Interceptor.attach(ExitProcess_p, {
+        onEnter(args) {
+            console.log('ExitProcess called');
+            const exit_val = args[0].toUInt32();
+            const proc = GetCurrentProcess();
+            TerminateProcess(proc, exit_val);
+        }
+    })
 } else {
     if (new_game_disc_prefix === null) {
         console.log('Game exe name mismatch. Refusing to patch.');
@@ -29,3 +45,6 @@ if (new_game_disc_prefix !== null && new_game_disc_prefix.length < GAME_DISC_PRE
         console.log('Game installation location does not fit within game disc prefix. Refusing to patch.');
     }
 }
+
+(global as any)._GetCurrentProcess = GetCurrentProcess;
+(global as any)._TerminateProcess = TerminateProcess;
